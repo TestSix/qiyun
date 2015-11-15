ea92e2b97113bc47b3e399fb0ebfdd5de36904ea
queran <canputi@163.com>
2015-11-15 13:01:18 +0800

    update

---
+++ searchstring.py
@@ -0,0 +1,21 @@
+#coding=utf-8
+
+import sys
+import linecache
+# 读去传入的参数 文件名 查找字符（未作校验）
+logname=sys.argv[1]
+strname=sys.argv[2]
+nw=0
+
+#读取文件，每行作为列表的形式
+filelist = linecache.getlines(logname)
+for list in filelist:
+	#查找字符串 如果判断如果不存在返回负一
+	if list.find(strname)!=-1:
+		#去掉换行符
+		lis= list.strip('\n')
+		nw=1
+		print lis
+if nw=0：
+	print '查找的字符不存在'
+#未判断传入的 文件名  字符串 是否符合规范   如果字符串是换行字符串如何处理
